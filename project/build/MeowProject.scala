d634f08cfd36b893c8111e48c05c5c374082ad1b
doug tangren <d.tangren@gmail.com>
2010-02-06 01:11:50 -0500

    added publishing definition

--- project/build/MeowProject.scala
+++ project/build/MeowProject.scala
@@ -4,4 +4,8 @@ class MeowProject(info: ProjectInfo) extends DefaultProject(info) {
   override def crossScalaVersions = Set("2.7.6","2.7.6", "2.7.7", "2.8.0.Beta1-RC1") 
   val stSnapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2-SNAPSHOT" % "test"
+  
+  override def managedStyle = ManagedStyle.Maven
+  val localRepo = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
+  val publishTo = localRepo
 }
