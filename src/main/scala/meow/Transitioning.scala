dfe3bb7bfde5984481e91996e04dc5f53f285115
doug tangren <d.tangren@gmail.com>
2010-04-17 23:38:20 -0400

    lines that actually returns 'lines' and not characters

--- src/main/scala/meow/Transitioning.scala
+++ src/main/scala/meow/Transitioning.scala
@@ -3,9 +3,42 @@ package meow
 object Transitioning {
   import scala.io.Source
   class TransitionalSource(src: Source) {
+    /** LineIterator is shim incompatible verions Source#getLines in Scala > 2.8 and < 2.8 */
+    class LineIterator(iter: BufferedIterator[Char], separator: String) extends Iterator[String] {
+      require(separator.length < 3, "Line separator may be 1 or 2 characters only.")
+      
+      private[this] val isNewline: Char => Boolean =
+        separator.length match {
+          case 1 => _ == separator(0)
+          case 2 => {
+            _ == separator(0) && iter.hasNext && {
+              val res = iter.head == separator(1) // peek ahead
+              if (res) { iter.next } // incr iter
+              res
+            }
+          }
+        }
+
+      private[this] val builder = new StringBuilder
+
+      private def buildingLine() = iter.next match {
+        case nl if(isNewline(nl)) => false
+        case ch =>  { 
+          builder append ch
+          true
+        }
+      }
+
+      def hasNext = iter.hasNext
+      def next = {
+        builder.clear
+        if(hasNext) while (hasNext && buildingLine()) {}
+        builder.toString
+      }
+    }
     /** Source#getLines compiles < 2.8 but does not in > 2.8 and visa versa
       * due to new default args in 2.8 impl */
-    def lines = src.toList.filter(!_.toString.equals(compat.Platform.EOL))
+    def lines = new LineIterator(src.buffered, compat.Platform.EOL)
   }
   implicit def src2transitionalSrc(src: Source) = new TransitionalSource(src)
 }
