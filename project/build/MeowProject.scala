5a90d5ba229171faeee78c1c61aa3ec9b2142d6a
doug tangren <d.tangren@gmail.com>
2010-06-01 22:38:08 -0700

    non-snapshot specs

--- project/build/MeowProject.scala
+++ project/build/MeowProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class MeowProject(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish {
   val stSnapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1-SNAPSHOT" % "test"
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("lessis repo", new java.io.File("/var/www/repo"))
